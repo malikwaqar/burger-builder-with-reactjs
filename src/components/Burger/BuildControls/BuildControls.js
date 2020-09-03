969b227153b6e223e49b580fb83a8ac07ae8a7b7
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-04 01:40:11 +0500

    Adding the Build Control Component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './BuildControls.css';
+
+const buildControls = () => (
+    <div className={classes.BuildControls}>
+
+    </div>
+);
+
+export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-    render() {
-        return(
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    render () {
+        return (
             <Aux>
-                <Burger />
-                <div>Burger Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
             </Aux>
         );
     }
