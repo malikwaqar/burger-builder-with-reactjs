ae7316e639517781e4e081b608a20f63910b77c1
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-03 18:56:15 +0500

    Creating Layout Component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <div>Tootbar, Sidedrawer, Backdrop</div>
-        <main>{props.children}</main>
+        <main className={classes.Content}>{props.children}</main>
     </Aux>
 );
 export default layout;
