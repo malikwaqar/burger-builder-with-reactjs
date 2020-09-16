59985dc7e283b91c0d23b3dd4482010a00137bd3
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-16 11:08:47 +0500

    Firebase deploy

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,12 +88,13 @@ componentDidMount () {
     }
 
     purchaseContinueHandler = () => {
+        // alert('You continue!');
         
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        queryParams.push('price=' + this.props.totalPrice);
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
